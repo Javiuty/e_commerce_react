f0ddc723eeba4d7177f297ddfc0aed906bb80a00
Javiuty <javiiescuadra@gmail.com>
2020-11-30 12:45:00 +0100

    navbar finished + clean code + calling commerceJS api

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,22 +9,34 @@ import {
   Typography,
 } from "@material-ui/core";
 import { ShoppingCart } from "@material-ui/icons";
-import classes from "*.module.css";
+
+import logo from "../../assets/commerce.png";
+import useStyles from "./styles";
 
 const Navbar = () => {
+  const classes = useStyles();
+
   return (
     <>
       <AppBar position="fixed" className={classes.appBar} color="inherit">
         <Toolbar>
           <Typography variant="h6" className={classes.title} color="inherit">
             <img
-              src={}
+              src={logo}
               alt="Commerce.js"
               height="25px"
               className={classes.image}
             />
             Commerce.js
           </Typography>
+          <div className={classes.grow} />
+          <div className={classes.button}>
+            <IconButton arial-label="Show cart items" color="inherit">
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
         </Toolbar>
       </AppBar>
     </>
