2e619bf5c9ca555e4e34747f7f208e129ac08b51
Javiuty <javiiescuadra@gmail.com>
2020-11-30 11:36:08 +0100

    creating files

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  IconBotton,
+} from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
+
+const Product = () => {
+  return <Card></Card>;
+};
+
+export default Product;
