a7cbecec0bc3cf4b32b6df6fc174d746056f98c9
Javiuty <javiiescuadra@gmail.com>
2020-12-01 13:25:54 +0100

    adding routing + shopping cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Container, Typography, Button, Grid } from "@material-ui/core";
 
 import useStyles from "./styles";
+import CardItem from "../Cart/CartItem/CartItem";
 
 const Cart = ({ cart }) => {
-  const isEmpty = !cart.line_items.length;
   const classes = useStyles();
 
   const EmptyCard = () => (
@@ -18,7 +18,7 @@ const Cart = ({ cart }) => {
       <Grid container spacing={3}>
         {cart.line_items.map((item) => (
           <Grid item xs={12} sm={4} key={item.id}>
-            <div>{item.name}</div>
+            <CardItem item={item} />
           </Grid>
         ))}
       </Grid>
@@ -50,13 +50,15 @@ const Cart = ({ cart }) => {
     </>
   );
 
+  if (!cart.line_items) return "Loading...";
+
   return (
     <Container>
       <div className={classes.toolbar} />
-      <Typography className={classes.title} variant="h3">
+      <Typography className={classes.title} variant="h3" gutterBottom>
         Your Shopping Cart
       </Typography>
-      {isEmpty ? <EmptyCard /> : <FilledCart />}
+      {!cart.line_items.length ? <EmptyCard /> : <FilledCart />}
     </Container>
   );
 };
